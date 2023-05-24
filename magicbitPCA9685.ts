817025027a25541d316915614baf3a80242d5a07
Ondrejoda <54633773+Ondrejoda@users.noreply.github.com>
2023-05-24 18:09:29 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAA
    AAAAAA

--- magicbitPCA9685.ts
+++ magicbitPCA9685.ts
@@ -67,26 +67,26 @@ namespace PCAmotor {
 
     let initialized = false
 
-    function i2cwrite(addr: number, reg: number, value: number) {
+    export function i2cwrite(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = value
         pins.i2cWriteBuffer(addr, buf)
     }
 
-    function i2ccmd(addr: number, value: number) {
+    export function i2ccmd(addr: number, value: number) {
         let buf = pins.createBuffer(1)
         buf[0] = value
         pins.i2cWriteBuffer(addr, buf)
     }
 
-    function i2cread(addr: number, reg: number) {
+    export function i2cread(addr: number, reg: number) {
         pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
         let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
         return val;
     }
 
-    function initPCA9685(): void {
+    export function initPCA9685(): void {
         i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
         setFreq(50);
         for (let idx = 0; idx < 16; idx++) {
@@ -95,7 +95,7 @@ namespace PCAmotor {
         initialized = true
     }
 
-    function setFreq(freq: number): void {
+    export function setFreq(freq: number): void {
         // Constrain the frequency
         let prescaleval = 25000000;
         prescaleval /= 4096;
@@ -111,7 +111,7 @@ namespace PCAmotor {
         i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
     }
 
-    function setPwm(channel: number, on: number, off: number): void {
+    export function setPwm(channel: number, on: number, off: number): void {
         if (channel < 0 || channel > 15)
             return;
         //serial.writeValue("ch", channel)
@@ -127,7 +127,7 @@ namespace PCAmotor {
         pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
     }
 
-    function setStepper(index: number, dir: boolean): void {
+    export function setStepper(index: number, dir: boolean): void {
         if (index == 1) {
             if (dir) {
                 setPwm(0, STP_CHA_L, STP_CHA_H);
@@ -155,7 +155,7 @@ namespace PCAmotor {
         }
     }
 
-    function stopMotor(index: number) {
+    export function stopMotor(index: number) {
         setPwm((index - 1) * 2, 0, 0);
         setPwm((index - 1) * 2 + 1, 0, 0);
     }
@@ -244,6 +244,23 @@ namespace PCAmotor {
         if (speed <= -4096) {
             speed = -4095
         }
+        if (index > 4 || index <= 0)
+            return
+        let pp = (index - 1) * 2
+        let pn = (index - 1) * 2 + 1
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
+    }
+
+    export function MotorRunModified(index: Motors, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        } 
         if (index > 4 || index <= 0)
             return
         let pp = (index - 1) * 2
